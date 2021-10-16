def4811e8e185387dfaf3459c691f57ae29c1964
Mario Merendino <mario.merendino@vimeo.com>
2021-10-16 14:17:14 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/people/:id', to: 'peoples#show'
-  get '/peoples/new', to: 'peoples#new'
-  post '/peoples/', to: 'peoples#create'
+  get '/person/new', to: 'peoples#new'
+  post '/people/', to: 'peoples#create'
   get '/people/', to: 'peoples#index'
   get '/people_mobile/:id', to: 'peoples#show_mobile'
 end
